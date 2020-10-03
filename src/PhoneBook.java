5eaa20fefcc095f4f05e8c82e152f64337ec72ac
Irina <irina.kanus@gmail.com>
2020-10-03 17:18:35 +0300

    Methods "checkName" and "formatName" added

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,19 +1,40 @@
+import java.util.Scanner;
+
 public class PhoneBook {
 
     public static void main(String[] args) {
-        //Добавить считывание ввода пользователя в цикле
+        Scanner scanner = new Scanner(System.in);
+        String [][] phonebook = new String[10][2];
+        System.out.println("Введите ФИО или номер");
+        boolean isCorrectName = false;
+        while (!isCorrectName) {
+            String name = scanner.nextLine();
+            isCorrectName = checkName(name);
+            if (!isCorrectName) System.out.println("Введите корректное имя!");
+        }
     }
 
+
     public static boolean checkPhoneNumber(String phoneNumber) {
+
         return true;
     }
 
     public static boolean checkName(String name) {
-        return true;
+        String[] words = name.trim().split(" ");
+        return (words.length == 3);
     }
 
     public static String formatName(String name) {
-        return "";
+        String formatted = name.toLowerCase();
+        String formattedName = "";
+        String[]words = formatted.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1);
+            formattedName = formattedName.concat(" ").concat(words[i]);
+        }
+
+        return formattedName.trim();
     }
 
     public static String formatPhoneNumber(String number) {
