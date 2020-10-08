d4a4ecf336cb825b2d9d981cfb07eb792845ee42
Irina <irina.kanus@gmail.com>
2020-10-09 01:09:08 +0300

    Classes "Application", "Sign.java", "StringFormatting" created

--- src/Application.java
+++ src/Application.java
@@ -4,26 +4,21 @@ import java.util.Scanner;
 public class Application {
     private String[][] book = new String[3][2];
     static boolean runFlag = true;
-
-    Processing processing = new Processing();
+    String name = "";
+    String number = "";
+    String[] currentLine = null;
     StringFormatting stringFormatting = new StringFormatting();
 
 
-    public void run(){
+    public void run() {
         Scanner scanner = new Scanner(System.in);
         int lineNumber = 0;
-        String number = "";
-        String name = "";
-        boolean isNumber = false;
-        boolean isName = false;
         System.out.println("Введите ФИО или номер, для вывода справочника введите list");
-        while (runFlag){
+        while (runFlag) {
             String scan = scanner.nextLine();
             switch (scan) {
                 case "list":
                     list(book);
-                    isName = false;
-                    isNumber = false;
                     System.out.println("Введите ФИО или номер, для вывода справочника введите list");
                     break;
                 case "exit":
@@ -33,59 +28,91 @@ public class Application {
                     switch (stringFormatting.parseInput(scan)) {
                         case NUMBER:
                             number = stringFormatting.formatPhoneNumber(scan);
-                            isNumber = true;
+                            String[] searchString;
+                            if (currentLine == null) {
+                                searchString = searchElement(book, number, 1);
+
+                                if (searchString == null) {
+                                    currentLine = new String[2];
+                                    currentLine[1] = number;
+                                    System.out.println("Введите, пожалуйста, ФИО");
+                                    break;
+                                } else {
+                                    System.out.println("Есть такая запись: \n" + searchString[0] + ":" + searchString[1] + "\nВведите ФИО или номер, для вывода справочника - list, выход - exit");
+                                    currentLine = null;
+                                }
+                            } else {
+                                searchString = searchElement(book, number, 1);
+                                if (searchString == null) {
+                                    currentLine[1] = number;
+                                    book = add(book, currentLine, lineNumber);
+                                    lineNumber++;
+                                    currentLine = null;
+                                } else {
+                                    System.out.println("Есть такая запись: \n" + searchString[0] + ":" + searchString[1] + "\nВведите ФИО или номер, для вывода справочника - list, выход - exit");
+                                    currentLine = null;
+                                }
+                            }
                             break;
+
+
                         case NAME:
                             name = stringFormatting.formatName(scan);
-                            isName = true;
+                            if (currentLine == null) {
+                                searchString = searchElement(book, name, 0);
+                                if (searchString == null) {
+                                    currentLine = new String[2];
+                                    currentLine[0] = name;
+                                    System.out.println("Введите, пожалуйста, номер");
+                                    break;
+                                } else {
+                                    System.out.println("Есть такая запись: \n" + searchString[0] + ":" + searchString[1] + "\nВведите ФИО или номер, для вывода справочника - list, выход - exit");
+                                    currentLine = null;
+                                }
+                            } else {
+                                searchString = searchElement(book, name, 0);
+                                if (searchString == null) {
+                                    currentLine[0] = name;
+                                    book = add(book, currentLine, lineNumber);
+                                    lineNumber++;
+                                    currentLine = null;
+                                } else {
+                                    System.out.println("Есть такая запись: \n" + searchString[0] + ":" + searchString[1] + "\nВведите ФИО или номер, для вывода справочника - list, выход - exit");
+                                    currentLine = null;
+                                }
+                            }
+
                             break;
                         case NONE:
                             System.out.println("Проверьте правильность ввода и повторите");
                             break;
                     }
-        }
 
-
-//      Вывод имеющейся записи или создание новой
-        if (isName&&isNumber) {
-            if (lineNumber == book.length-1) {
-                book = bookExtend(book);
-            }
-            String[] line = new String[2];
-            line[0] = name;
-            line[1] = number;
-            book[lineNumber] = line;
-            isName = false;
-            isNumber = false;
-            lineNumber++;
-            System.out.println("Записал");
-            System.out.println("Введите ФИО или номер, для вывода справочника введите list");
-
-        } else {
-            if (isName) {
-                System.out.println(processing.NameSearch(book, name));
-                isNumber = false;
-            } else {
-                if (isNumber) {
-                    System.out.println(processing.NumberSearch(book, number));
-                    isName = false;
-                }
             }
         }
     }
+
+    public String[][] add(String[][] book, String[] currentLine, int lineNumber) {
+        if (lineNumber == book.length - 1) {
+            book = bookExtend(book);
+        }
+        book[lineNumber] = currentLine;
+        System.out.println("Записал");
+        System.out.println("Введите ФИО или номер, для вывода справочника введите list");
+        return book;
     }
 
     private void list(String[][] book) {
         for (String[] strings : book) {
             if (strings == null) continue;
             else
-            System.out.println(strings[0] + ":" + strings[1]);
+                System.out.println(strings[0] + ":" + strings[1]);
         }
     }
 
     public static String[][] bookExtend(String[][] book) {
-        String[][] book1 = new String[book.length*2][2];
-        for (int i = 0; i < book1.length; i++){
+        String[][] book1 = new String[book.length * 2][2];
+        for (int i = 0; i < book1.length; i++) {
             for (int j = 0; j < 2; j++) {
                 book1[i][j] = "";
             }
@@ -93,4 +120,19 @@ public class Application {
         book1 = Arrays.copyOf(book, book1.length);
         return book1;
     }
+
+    public static String[] searchElement(String[][] book, String name, int position) {
+        String[] result = new String[2];
+        for (String[] strings : book) {
+            if (strings == null) continue;
+            if (name.equals(strings[position])) {
+                return strings;
+            } else {
+                result = null;
+            }
+
+        }
+        return result;
+    }
+
 }
